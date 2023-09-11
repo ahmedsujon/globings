521ed4a0068aec24081098f39167aa0ec2a2ffcf
nz-hridoy <nayeemuzaman05@gmail.com>
2023-09-11 19:49:12 +0600

    admin + user logout function

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    public function adminLogout()
+    public function adminLogout(Request $request)
     {
-        
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+
+    public function userLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        return redirect()->route('app.index');
     }
 }
