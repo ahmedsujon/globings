70be9ad8dc5bc0dcda9502a11f20a6baea9a1964
nz-hridoy <nayeemuzaman05@gmail.com>
2023-10-13 20:11:45 +0600

    Category Table Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(PackageTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
     }
 }
