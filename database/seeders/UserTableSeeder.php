743da1a14787b21b8ba48242f9e3257006f563d2
nz-hridoy <nayeemuzaman05@gmail.com>
2023-10-13 20:33:06 +0600

    user seeder update

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -19,13 +19,26 @@ class UserTableSeeder extends Seeder
         if (!$getUser) {
             $user = new User();
             $user->first_name = 'Globings';
-            $user->last_name = 'user';
+            $user->last_name = 'User';
             $user->email = 'user@example.com';
             $user->phone = '01700000000';
             $user->password = Hash::make('12345678');
             $user->avatar = 'assets/images/avatar.png';
             $user->account_type = 'private';
             $user->save();
+        }
+
+        $getShop = User::where('email', 'shop@example.com')->first();
+        if (!$getShop) {
+            $shop = new User();
+            $shop->first_name = 'Test Shop';
+            $shop->last_name = 'user';
+            $shop->email = 'shop@example.com';
+            $shop->phone = '01700000000';
+            $shop->password = Hash::make('12345678');
+            $shop->avatar = 'assets/images/avatar.png';
+            $shop->account_type = 'professional';
+            $shop->save();
         }
     }
 }
