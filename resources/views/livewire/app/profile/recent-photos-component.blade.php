d5c6507f1206cf8644dc99b6658a0a23a8e456f6
ahmedsujon <sujonahmed424@gmail.com>
2023-11-05 21:04:17 +0600

    recent photos

--- resources/views/livewire/app/profile/recent-photos-component.blade.php
+++ resources/views/livewire/app/profile/recent-photos-component.blade.php
@@ -2,71 +2,28 @@
     <main>
         <!-- Recent Photos Modal  -->
         <div class="pb-100">
-          <div class="container">
-            <div class="recent_photos_modal">
-              <div class="bing_back_area">
-                <div class="d-flex align-items-center flex-wrap g-xl">
-                  <h4 class="notification_title">Recent Photos</h4>
+            <div class="container">
+                <div class="recent_photos_modal">
+                    <div class="bing_back_area">
+                        <div class="d-flex align-items-center flex-wrap g-xl">
+                            <h4 class="notification_title">Recent Photos</h4>
+                        </div>
+                    </div>
+                    @foreach ($random_one as $post)
+                        <button type="button" class="top_img recentPhotoBtn">
+                        <img src="{{ asset('assets/app/images/others/image_gallery1.png') }}" alt="gallery image" />
+                    </button>
+                    @endforeach
+                    
+                    <div class="gallery_grid mt-24">
+                        @foreach ($posts as $post)
+                            <button type="button" class="gallery_img recentPhotoBtn">
+                            <img src="{{ asset('assets/app/images/others/image_gallery3.png') }}" alt="gallery image" />
+                        </button>
+                        @endforeach
+                    </div>
                 </div>
-              </div>
-              <button type="button" class="top_img recentPhotoBtn">
-                <img
-                  src="{{ asset('assets/app/images/others/image_gallery1.png') }}"
-                  alt="gallery image"
-                />
-              </button>
-              <div class="gallery_grid mt-24">
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery2.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery3.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery4.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery5.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery6.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery7.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery8.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-                <button type="button" class="gallery_img recentPhotoBtn">
-                  <img
-                    src="{{ asset('assets/app/images/others/image_gallery9.png') }}"
-                    alt="gallery image"
-                  />
-                </button>
-              </div>
             </div>
-          </div>
         </div>
-      </main>
+    </main>
 </div>
