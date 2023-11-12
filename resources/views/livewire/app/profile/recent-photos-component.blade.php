a73e3d5fa523449e83a3cf8a03c7e7fe59b46cc2
ahmedsujon <sujonahmed424@gmail.com>
2023-11-12 06:37:05 +0600

    change

--- resources/views/livewire/app/profile/recent-photos-component.blade.php
+++ resources/views/livewire/app/profile/recent-photos-component.blade.php
@@ -1,29 +1,27 @@
 <div>
-    <main>
-        <!-- Recent Photos Modal  -->
-        <div class="pb-100">
-            <div class="container">
-                <div class="recent_photos_modal">
-                    <div class="bing_back_area">
-                        <div class="d-flex align-items-center flex-wrap g-xl">
-                            <h4 class="notification_title">Recent Photos</h4>
-                        </div>
+    <!-- Recent Photos Modal  -->
+    <div class="pb-100">
+        <div class="container">
+            <div class="recent_photos_modal">
+                <div class="bing_back_area">
+                    <div class="d-flex align-items-center flex-wrap g-xl">
+                        <h4 class="notification_title">Recent Photos</h4>
                     </div>
-                    @foreach ($random_one as $post)
-                        <button type="button" class="top_img recentPhotoBtn">
+                </div>
+                @foreach ($random_one as $post)
+                    <button type="button" class="top_img recentPhotoBtn">
                         <img src="{{ asset('assets/app/images/others/image_gallery1.png') }}" alt="gallery image" />
                     </button>
-                    @endforeach
-                    
-                    <div class="gallery_grid mt-24">
-                        @foreach ($posts as $post)
-                            <button type="button" class="gallery_img recentPhotoBtn">
+                @endforeach
+
+                <div class="gallery_grid mt-24">
+                    @foreach ($posts as $post)
+                        <button type="button" class="gallery_img recentPhotoBtn">
                             <img src="{{ asset('assets/app/images/others/image_gallery3.png') }}" alt="gallery image" />
                         </button>
-                        @endforeach
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
-    </main>
+    </div>
 </div>
