838b81d18e2f2706abf9359cadfaa25462e3d699
nz-hridoy <nayeemuzaman05@gmail.com>
2023-11-16 02:14:30 +0600

    error fix

--- resources/views/livewire/app/scanner/profile-scanner-component.blade.php
+++ resources/views/livewire/app/scanner/profile-scanner-component.blade.php
@@ -3,14 +3,24 @@
         .scanner {
             text-align: center;
             font-size: 15px;
-            height: 70vh;
+        }
+        .scanned_cont {
+            text-align: center;
+            font-size: 17px;
+            margin-top: 35px;
         }
     </style>
 
     <div class="scanner">
         <video id="preview" style="width: 100%;"></video>
-
-        <div class="scaning_loader" style="margin-top: 25px;"></div>
+    </div>
+    <div class="scanned_cont d-none">
+        <div class="scaning_loader">
+            <i class="fa fa-spinner fa-spin" style="font-size: 30px; margin-bottom: 25px;"></i><br>
+            Validating QrCode
+            <br>
+            Please wait...
+        </div>
     </div>
 
 </div>
@@ -23,6 +33,8 @@
         });
 
         scanner.addListener('scan', function(content) {
+            $('.scanner').addClass('d-none');
+            $('.scanned_cont').removeClass('d-none');
             var data = content;
 
             $.ajax({
@@ -33,27 +45,30 @@
                     "data" : data
                 },
                 beforeSend: function() {
-                    $(".scaning_loader").html('<i class="fa fa-spinner fa-spin"></i> Scaning...');
                 },
                 success: function(data) {
                     if(data['message'] == 'insufficient_bings'){
                         toast_msg('Not enough bings balance', 'error');
-                        $(".scaning_loader").html('');
+                        $('.scanner').removeClass('d-none');
+                        $('.scanned_cont').addClass('d-none');
                     }
                     if(data['message'] == 'second_scan'){
                         toast_msg('Already scanned on this shop', 'error');
-                        $(".scaning_loader").html('');
+                        $('.scanner').removeClass('d-none');
+                        $('.scanned_cont').addClass('d-none');
                     }
                     if(data['message'] == 'error_self_scan'){
                         toast_msg('Can not scan self code!', 'error');
-                        $(".scaning_loader").html('');
+                        $('.scanner').removeClass('d-none');
+                        $('.scanned_cont').addClass('d-none');
                     }
                     if(data['message'] == 'invalid_qr_code'){
                         toast_msg('Invalid QrCode', 'error');
-                        $(".scaning_loader").html('');
+                        $('.scanner').removeClass('d-none');
+                        $('.scanned_cont').addClass('d-none');
                     }
                     if(data['message'] == 'scan_successful'){
-                        toast_msg('Scan successful, redirecting...', 'success');
+                        toast_msg('Validation successful, redirecting...', 'success');
 
                         window.location.href = "{{ URL::to('/') }}/scanner/scan-success?scan_id="+data['scan_id'];
                     }
