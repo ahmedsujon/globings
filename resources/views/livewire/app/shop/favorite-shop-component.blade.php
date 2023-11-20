7dca97048117cd4f02bd2fc02e5006bfb5df5d27
nz-hridoy <nayeemuzaman05@gmail.com>
2023-11-20 11:26:39 +0600

    my bookmarks

--- resources/views/livewire/app/shop/favorite-shop-component.blade.php
+++ resources/views/livewire/app/shop/favorite-shop-component.blade.php
@@ -1,74 +1,38 @@
 <div>
-    <!-- Company Location Section  -->
     <section class="company_location_wrapper">
-        <div class="location_header">
+        <div class="location_area location_all_shop_area">
             <div class="container">
-                <form action="" class="location_form_area">
-                    <h4>Your Location</h4>
-                    <div class="search_grid">
-                        <div class="position-relative">
-                            <select id="locationSearchSelect" wire:model="searchTerm">
-                                <option value=""></option>
-                                @foreach ($shops as $shop)
-                                    <option value="{{ $shop->city }}">{{ $shop->city }}</option>
-                                @endforeach
-                            </select>
-                            <img src="{{ asset('assets/app/icons/select_arrow_icon.svg') }}" alt="select arrow"
-                                class="arrow_icon" />
-                        </div>
-                        <a href="{{ route('app.map.view') }}" class="map_btn">
-                            <img src="{{ asset('assets/app/icons/map_icon.svg') }}" alt="map icon" />
-                        </a>
-                    </div>
-                </form>
-                <div class="category_slider_area border-0" id="headerCategorySlider">
-                    <div class="swiper">
-                        <div class="swiper-wrapper">
-                            @foreach ($categories as $category)
-                                <div class="swiper-slide">
-                                    <a href="{{ route('app.home') }}?category={{ $category->id }}"
-                                        class="category_item {{ request()->get('category') == $category->id ? 'active_category' : '' }}">
-                                        <img src="{{ asset($category->icon) }}" alt="category icon" />
-                                        <h4>{{ $category->name }}</h4>
-                                    </a>
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="location_area">
-            @foreach ($shops as $shop)
-                <div class="location_item">
-                    <div class="position-relative">
-                        <a href="{{ route('app.shopProfile', ['user_id' => $shop->user_id]) }}">
-                            <img src="{{ asset('assets/app/images/post/location_img.png') }}" alt="post image"
-                                class="post_img" />
-                        </a>
-                        <div class="info_area">
-                            <div class="container">
-                                <div class="d-flex-between">
-                                    <h4>{{ $shop->shop_category }}</h4>
-                                    <div class="ratting_area">
-                                        <img src="{{ asset('assets/app/icons/star_small_icon.svg') }}"
-                                            alt="star small icon" />
-                                        <span>4.5</span>
+                <h4 class="notification_title">My Bookmarks</h4>
+                <hr>
+
+                @if ($shops->count() > 0)
+                    @foreach ($shops as $shop)
+                        <div class="location_item">
+                            <div class="position-relative">
+                                <img src="{{ asset($shop->cover_photo) }}" alt="post image" class="post_img" />
+                                <div class="info_area">
+                                    <div class="container">
+                                        <div class="d-flex-between">
+                                            <h4>{{ $shop->shop_category }}</h4>
+                                            <div class="ratting_area">
+                                                <img src="{{ asset('assets/app/icons/star_small_icon.svg') }}" alt="star small icon" />
+                                                <span>{{ avgShopReview($shop->id) }}</span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
+                            <div class="content">
+                                <a href="{{ route('app.shopProfile', ['user_id'=>$shop->user_id]) }}"><h3>{{ $shop->name }}</h3></a>
+                                <h5>{{ $shop->address }} <span> . Open now</span></h5>
+                            </div>
                         </div>
-                    </div>
-                    <div class="container">
-                        <div class="content">
-                            <h3><a
-                                    href="{{ route('app.shopProfile', ['user_id' => $shop->user_id]) }}">{{ $shop->name }}</a>
-                            </h3>
-                            <h5>324 ft <span>. Open now</span></h5>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+                    @endforeach
+                @else
+
+                @endif
+            </div>
         </div>
     </section>
 </div>
