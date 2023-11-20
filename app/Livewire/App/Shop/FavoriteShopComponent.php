9782a198e9dfb3097bdee854914ffd18d29e2409
nz-hridoy <nayeemuzaman05@gmail.com>
2023-11-20 14:09:05 +0600

    update + fix

--- app/Livewire/App/Shop/FavoriteShopComponent.php
+++ app/Livewire/App/Shop/FavoriteShopComponent.php
@@ -2,14 +2,15 @@
 
 namespace App\Livewire\App\Shop;
 
-use App\Models\Shop;
-use Livewire\Component;
 use App\Models\Category;
+use App\Models\Shop;
+use App\Models\ShopBookmark;
+use Livewire\Component;
 
 class FavoriteShopComponent extends Component
 {
     public $categories, $pagination_value = 50, $searchTerm;
-    
+
     public function mount()
     {
         $this->categories = Category::where('status', 1)->orderBy('name', 'ASC')->get();
@@ -17,9 +18,9 @@ class FavoriteShopComponent extends Component
 
     public function render()
     {
-        $shops = Shop::orderBy('id', 'DESC')
-        ->where('city', 'like', '%' . $this->searchTerm . '%')
-        ->paginate($this->pagination_value);
-        return view('livewire.app.shop.favorite-shop-component', ['shops'=>$shops])->layout('livewire.app.layouts.base');
+        $bookmarks = ShopBookmark::where('user_id', user()->id)->pluck('shop_id')->toArray();
+
+        $shops = Shop::whereIn('id', $bookmarks)->orderBy('id', 'DESC')->where('city', 'like', '%' . $this->searchTerm . '%')->paginate($this->pagination_value);
+        return view('livewire.app.shop.favorite-shop-component', ['shops' => $shops])->layout('livewire.app.layouts.base');
     }
 }
