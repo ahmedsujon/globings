c0876dce70ef2567e686516a186b94dcd4f64896
ahmedsujon <sujonahmed424@gmail.com>
2024-01-06 09:26:42 +0600

    reward

---
+++ app/Livewire/Admin/Reward/RewardRequestsComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Reward;
+
+use Livewire\Component;
+
+class RewardRequestsComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.reward.reward-requests-component')->layout('livewire.admin.layouts.base');
+    }
+}
